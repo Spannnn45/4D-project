bc5347224fc9eef826d45701eeb3477cc091be41
svante <svante.spannar@gmail.com>
2021-02-14 12:38:41 +0100

    update

--- Project3d.py
+++ Project3d.py
@@ -7,6 +7,10 @@ points = [[[1], [1], [1]]]
 distance = 4
 clock = pygame.time.Clock()
 
+faces = []
+
+projectedPoint = [i for i in range(0, 16)]
+
 rotation_x = [[1, 0, 0],
               [0, math.cos(0), -math.sin(0)],
               [0, math.sin(0), math.cos(0)]]
@@ -56,6 +60,13 @@ def set_scale(scales):
     global scale
     scale = scales
 
+def set_faces(face):
+    global faces
+    faces = face
+
+def connect(a, b):
+    pygame.draw.line(display, black, ((a[0][0]*scale)+1920//2, (a[1][0])+1080//2),((b[0][0]*scale)+1920//2, (b[1][0])+1080//2), 1)
+
 pygame.display.set_caption("Well hello there")
 display = pygame.display.set_mode((1920, 1080))
 
@@ -64,13 +75,14 @@ display.fill(white)
 def draw():
     clock.tick(60)
     display.fill(white)
-    for point in points:
-        rotated = matMult(rotation_x, point)
+    for i in range(0, len(points)):
+        rotated = matMult(rotation_x, points[i])
         rotated = matMult(rotation_y, rotated)
         rotated = matMult(rotation_z, rotated)
 
         projected = matMult([[1 / (distance - rotated[2][0]), 0, 0], [0, 1 / (distance - rotated[2][0]), 0]], rotated)
-
+        projectedPoint[i] = projected
         pygame.draw.circle(display, blue, ((projected[0][0] * scale)+1920//2, (projected[1][0] * scale)+1080//2), 10)
 
+    connect(projectedPoint[0], projectedPoint[1])
     pygame.display.update()
